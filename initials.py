b75fc81fc0068e0d55ee408e2737aa78e14887bb
ntiBoateng <gideon.boateng@amalitech.org>
2022-01-14 12:55:12 -0800

    Initial commit

---
+++ initials.py
@@ -0,0 +1,9 @@
+#NTI Boateng Gideon
+#Imagining myself meeting the girl of my dream
+print("N    N")
+print("N N  N")
+print("N  N N")
+print("N   NN")
+print("N    N")
+print("N    N")
+print("N    N")
